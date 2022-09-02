1f7d90e290b25c497ab2f74afcb9bc668b0369e7
Grisha_Pinigin <pin.pinigin@mail.ru>
2022-09-02 13:06:55 +0500

    Задача 38

---
+++ DZ_38/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
+// [3 7 22 2 78] -> 76
+
+double [] arr =  {3, 7, 22, 2, 78};
+//double [] arr =  {3, 7, 22, 0.2, 78, 83.8};
+
+double max = arr[1];
+double min = arr[0];
+double result = 0;
+
+for (int i = 0; i < arr.Length; i++)
+{
+    if(min >= arr[i])
+    {
+        min = arr[i]; 
+    }
+    if(max <= arr[i])
+    {
+        max = arr[i];
+    } 
+    result = max - min;
+}
+Console.WriteLine();
+Console.WriteLine("[{0}] -> " + result, String.Join("; ", arr));
+Console.WriteLine("Максимальный элемент: " + max);
+Console.WriteLine("Минимальный элемент: " + min);
+Console.WriteLine("Разница между элементами: " + result);
+Console.WriteLine();
